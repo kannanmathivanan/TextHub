956423b4a2ad32a88bf28934a278dd39e8561ee9
kannanmathivanan <131963619+kannanmathivanan@users.noreply.github.com>
2024-12-25 14:50:49 +0800

    Update App.jsx

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -6,13 +6,60 @@ import Home from './Home';
 import About from './About';
 import Searchpost from './Searchpost';
 import { Routes,Route, useNavigate} from 'react-router-dom';
-import api from './api/posts';
 import Profilepage from './Profilepage';
 
 
 function App() {
 
-const[posts,setPosts]=useState([])
+const[posts,setPosts]=useState([ {
+    id:1,
+    profile:"my photo2.jpg",
+    mode:true,
+    name:"Kannan",
+    subject:"Motivational Qutoes",
+    post:"Dream big and work hard.Every step forward takes you closer to your goal.Mistakes are lessons in disguise—embrace them.Believe in yourself and your journey.Success is yours if you never give up!",
+},
+{
+  id:2,
+  profile:"vinoth1.jpg",
+  mode:false,
+  name:"VinothKumar",
+  subject:"Hiring!!!",
+  post:"Are you passionate about building exceptional websites and creating seamless user experiences? Join our dynamic team as a Web Developer! We’re looking for a creative and detail-oriented professional who thrives in a fast-paced environment and enjoys bringing innovative ideas to life.",
+},
+{
+  id:3,
+  profile:"sankar.jpg",
+  mode:false,
+  name:"Sankar",
+  subject:"Good Morning Whishes",
+  post:"Good morning! Start your day with a smile and a heart full of gratitude Each sunrise is a chance to begin again.Make today beautiful and bright.Have a wonderful day ahead!"
+},
+{
+  id:4,
+  profile:"aravinth.jpg",
+  mode:true,
+  name:"Aravinth",
+  subject:"Love Qutoes",
+  post:"Love is not about how many days, months, or years you’ve been together.It’s about how much you love each other every single day.You are my sunshine on cloudy days.With you, every moment feels magical.",
+},
+{
+  id:5,
+  profile:"siva.jpg",
+  mode:false,
+  name:"Siva",
+  subject:"Love Qutoes",
+  post:"In your arms, Ive found my forever home.Your love is the melody my heart dances to.No matter what life throws our way, I’m with you always.Together, we are unbreakable.I love you morethan words can say",
+  
+},
+{
+  id:6,
+  profile:"sanjai.jpg",
+  mode:true,
+  name:"Sanjai",
+  subject:"Thalapathy",
+  post:"Vijay isnt just a name; its an emotion for millions of fans who see him as a symbol of hard work and perseverance",
+},])
 const [search,setSearch] = useState('');
 const [newProfile,setnewProfile]=useState('');
 const [newName,setnewName]=useState('');
@@ -20,20 +67,13 @@ const [newSubject,setnewSubject]=useState('');
 const [newPost,setnewPost]=useState('');
 const navigate =  useNavigate();
 
-useEffect( ()=>{
-  const fetchPosts = async ()=>{
-     const response = await api.get('/posts');
-     setPosts(response.data)
-  }
-  fetchPosts();
-},[])
+
 
 const handleSubmit = async (e)=>{
     e.preventDefault();
     const id = posts.length?posts[posts.length-1].id+1:1;
     const addPosts = {id,mode:true,profile:newProfile, name:newName, subject:newSubject , post:newPost };
-    const response = await api.post('/posts',addPosts);
-    const allPosts = [...posts,response.data];
+    const allPosts = [...posts,addPosts];
     setPosts(allPosts);
     setnewProfile("");
     setnewName("");
